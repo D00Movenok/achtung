ff57983dd222c657daa8753f76e945f8368d6228
D00Movenok <egor.gennadjev@yandex.ru>
2021-04-24 22:55:26 +0300

    added session maker

--- catcher/common/database.py
+++ catcher/common/database.py
@@ -1,6 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 engine = create_async_engine(
     'sqlite+aiosqlite:///database.db',
     echo=True,
+)
+
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
 )
